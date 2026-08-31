421b58e7a71770751b3ebb6304306f0cb6e5e5ba
Vincent Chen <vincentchen737@gmail.com>
2026-08-31 00:32:04 +0000

    Changed variable to Z

--- streamlit_app.py
+++ streamlit_app.py
@@ -307,7 +307,7 @@ with tab1:
     tau = st.slider(r"Time Delay ($\tau$)", 1, 50, 15, help="Time lag used for Takens' state space reconstruction.")
     
     # Generate delayed vectors for 3D embedding
-    x_vals = df_base1['Y'].values
+    x_vals = df_base1['Z'].values
     x_t = x_vals[:-2*tau]
     x_t_tau = x_vals[tau:-tau]
     x_t_2tau = x_vals[2*tau:]
@@ -316,10 +316,10 @@ with tab1:
     fig_embed = plt.figure(figsize=(8, 6))
     ax_embed = fig_embed.add_subplot(111, projection='3d')
     ax_embed.plot(x_t, x_t_tau, x_t_2tau, lw=0.7, color='purple')
-    ax_embed.set_xlabel('X(t)')
-    ax_embed.set_ylabel(f'X(t + {tau})')
-    ax_embed.set_zlabel(f'X(t + {2*tau})')
-    ax_embed.set_title("3D Delay Reconstructed Phase Space from X")
+    ax_embed.set_xlabel('Z(t)')
+    ax_embed.set_ylabel(f'Z(t + {tau})')
+    ax_embed.set_zlabel(f'Z(t + {2*tau})')
+    ax_embed.set_title("3D Delay Reconstructed Phase Space from Z")
     st.pyplot(fig_embed)
     plt.close(fig_embed)
     
